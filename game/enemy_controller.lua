eb549a90c4cbdcbb5a89de831289023386d9cf12
Gustavo Henrique Müller <46305883+GustavoHenriqueMuller@users.noreply.github.com>
2024-10-06 23:04:44 -0300

    Commit

--- game/enemy_controller.lua
+++ game/enemy_controller.lua
@@ -5,7 +5,7 @@ require("entities.Scorpion")
 EnemyController = Controller:extend()
 
 function EnemyController:init()
-    self:base_init(15, 6, 1.15)
+    self:base_init(0, 8, 1.15)
 end
 
 function EnemyController:update(game)
@@ -17,9 +17,9 @@ function EnemyController:update(game)
         -- Spawn random entity in random lane. Else, save gold.
         local lane_index = math.random(1, #game.lanes)
 
-        local roll_2 = math.random(1, 3)
+        local roll_2 = math.random(1, 4)
 
-        if roll_2 > 2 then
+        if roll_2 >= 2 then
             -- Spawn biggest creature money can buy.
             for i = #UI_BUTTON_ENTITIES, 1, -1 do
                 local entity_class = UI_BUTTON_ENTITIES[i]
--- game/entities/snail.lua
+++ game/entities/snail.lua
@@ -4,8 +4,8 @@ require("sprite")
 Snail = Entity:extend()
 Snail.database = {}
 Snail.database.speed = 0.8
-Snail.database.hp = 270
-Snail.database.damage = 10
+Snail.database.hp = 340
+Snail.database.damage = 7
 Snail.database.base_damage = 100
 Snail.database.cost = 220
 Snail.database.attack_type = ATTACK_TYPE.MELEE
